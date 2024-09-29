a7ed54f0c7619cb57b63bbcfa21e90b50371102c
Danilo Arantes <daniloarantes@gmail.com>
2024-09-29 16:04:25 -0300

    Adicionada primeira função

--- index.php
+++ index.php
@@ -7,7 +7,31 @@
 </head>
 <body>
 
+<h1>Estudo Funções no PHP</h1>
+<p><strong>Objetivo:</strong> consolidar a criação e uso de funções utilizando a linguagem PHP.</p>
 
+<h2>Função simples</h2>
+
+<form action="index.php">
+    <label for="num">Número:</label>
+    <input type="text" id="num" name="num">
+    <button>Calcular</button>
+</form>
+
+<?php
+    if (isset($_GET['num'])) {
+        $num = $_GET["num"];
+    }
+
+    // FUNÇÃO SIMPLES
+    function quadrado($num) {
+        echo "$num ao quadrado é " . $num * $num;
+    }
+
+    // Função simples
+    quadrado($num);
+
+?>
 
 </body>
 </html>
